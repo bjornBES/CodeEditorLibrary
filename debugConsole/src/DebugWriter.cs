3057e1394f00bbfba999ad76d8e60c052e9c02e6
BjornBEs <bjorn@r66.dk>
2025-09-23 15:50:15 +0200

    final and done with Avalonia edit and Textmate

---
+++ debugConsole/src/DebugWriter.cs
@@ -0,0 +1,86 @@
+
+using System.Text;
+namespace lib.debug;
+public static class DebugWriter
+{
+    private static TextWriter originalWriter;
+
+    private static Dictionary<string, string> AltLogFileName = new Dictionary<string, string>();
+    private static Dictionary<string, string> AltLogModule = new Dictionary<string, string>();
+
+    public static void Initialize(TextWriter writer)
+    {
+        originalWriter = writer;
+        Console.SetOut(new InterceptWriter(writer));
+    }
+
+    public static void AddModule(string module, string FileName, string logModuleDirectory)
+    {
+        AltLogFileName.Add(module, FileName);
+        AltLogModule.Add(module, logModuleDirectory);
+    }
+
+    public static void AddModules(string FileName, string logModuleDirectory, params string[] modules)
+    {
+        foreach (var module in modules)
+        {
+            AltLogFileName.Add(module, FileName);
+            AltLogModule.Add(module, logModuleDirectory);
+        }
+    }
+
+    public static void AddModuleToLog(string module, string FileName, string logModuleDirectory)
+    {
+        AltLogFileName.Add(module, FileName);
+        AltLogModule.Add(module, logModuleDirectory);
+    }
+
+    public static void AddModulesToLog(string parentModule, params string[] modules)
+    {
+        string FileName = AltLogFileName[parentModule];
+        string logModuleDirectory = AltLogModule[parentModule];
+        
+        foreach (var module in modules)
+        {
+            AltLogFileName.Add(module, FileName);
+            AltLogModule.Add(module, logModuleDirectory);
+        }
+    }
+
+    public static void Clean()
+    {
+        Directory.GetFiles("logs", "debug_log*console.log", SearchOption.AllDirectories).ToList().ForEach(File.Delete);
+        File.Delete("logs/debug.log");
+
+        Directory.EnumerateDirectories("logs", "*", SearchOption.AllDirectories).ToList().ForEach(Directory.Delete);
+    }
+#nullable enable
+    public static void WriteLine(string module, string? value)
+    {
+        originalWriter.WriteLine($@"[{module}] {DateTime.Now:HH:mm:ss} {value}");
+        AddToLogFile(module, nameof(WriteLine), value + Environment.NewLine);
+        // Additionally log to a debug log file or system if needed
+    }
+
+    public static void Write(string module, string? value)
+    {
+        originalWriter.Write($@"[{module}] {DateTime.Now:HH:mm:ss} {value}");
+        AddToLogFile(module, nameof(Write), value + Environment.NewLine);
+        // Additionally log to a debug log file or system if needed
+    }
+
+    static void AddToLogFile(string module, string func, string? value)
+    {
+        string LogFileName = AltLogFileName[module];
+        string LogModule = AltLogModule[module];
+        if (!string.IsNullOrEmpty(LogFileName))
+        {
+            if (!Directory.Exists($"logs/{LogModule}"))
+            {
+                Directory.CreateDirectory($"logs/{LogModule}");
+            }
+            File.AppendAllText($"logs/{LogModule}/debug_{LogFileName}.log", $"{module} STDOUT {func} {DateTime.Now:HH:mm:ss} {value}", Encoding.UTF8);
+            File.AppendAllText($"logs/debug_log_all_console.log", $"{module} STDOUT {func} {DateTime.Now:HH:mm:ss} {value}", Encoding.UTF8);
+        }
+    }
+}
